36e81e8690bdf85c4868b90b89b2774db74776d9
sekikawa <skkwsh@gmail.com>
2019-12-17 12:21:38 +0900

    デバッグコード、コメントアウト README更新

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -4,18 +4,16 @@ class GameManager extends GameObject
     static isTitleEnd : boolean = false;
     public pause : boolean = false;
     public mode : number = 0;
-    //public score : number = 0;
     public timer : egret.Timer = null;
     private text : egret.TextField = null;
-    //private isStop : boolean = false;
 
     constructor() {
         super();
         GameManager.I = this;
 
         // timer
-        this.text = Util.myText(550, 5, "0", 50, 0.5, 0xc0c0c0, false);
-        GameObject.uiDisplay.addChild( this.text );
+        //this.text = Util.myText(550, 5, "0", 50, 0.5, 0xc0c0c0, false);
+        //GameObject.uiDisplay.addChild( this.text );
 
 
         if( !GameManager.isTitleEnd ){
@@ -30,44 +28,21 @@ class GameManager extends GameObject
     {
         this.pause = false;
 
-        this.timer = new egret.Timer(1000, 0);
-        //this.timer.addEventListener(egret.TimerEvent.TIMER, this.timerFunc, this);
-        this.timer.start();
+        //this.timer = new egret.Timer(1000, 0);
+        //this.timer.start();
 
         ObstacleGen.I.start();
     }
 
 
     onDestroy() {
-        GameObject.uiDisplay.removeChild( this.text );
+        //GameObject.uiDisplay.removeChild( this.text );
         this.text = null;
         
-        //this.timer.removeEventListener(egret.TimerEvent.TIMER, this.timerFunc, this);
         this.timer = null;
         GameManager.I = null;
     }
 
-    timerFunc(){}
-
-    onButton()
-    {
-        GameManager.I.timer.reset();
-        GameManager.I.timer.start();
-        
-        /*
-        if( GameManager.I.timer.running ){
-            GameManager.I.timer.stop();
-        }
-        else{
-            GameManager.I.timer.start();
-        }*/
-
-    }
-
-    onButton2()
-    {
-        GameManager.isTitleEnd = true;
-    }
 
     updateContent()
     {
@@ -75,8 +50,8 @@ class GameManager extends GameObject
             return;
         }
         
-        if( this.text != null ){
-            this.text.text = this.timer.currentCount.toString();
-        }
+        //if( this.text != null ){
+        //    this.text.text = this.timer.currentCount.toString();
+        //}
     }
 }
